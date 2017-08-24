b6c9ea4fda4b28e38dea9df9a23695a0e74e9a76
Artem Vasiliev <nooble6981@gmail.com>
2017-08-24 18:13:20 +0300

    entry2

--- wp-content/themes/unicon/framework/inc/post-format/entry.php
+++ wp-content/themes/unicon/framework/inc/post-format/entry.php
@@ -16,22 +16,7 @@
                 <h1><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__('Permalink to %s', 'minti'), the_title_attribute('echo=0') ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
             <?php } ?>
         </div>
-  <?php if (!is_single() || (is_single() && get_post_meta( get_the_ID(), 'minti_hideimage', true ) == false)) { ?>
-        <?php if ( has_post_thumbnail() ) { ?>
-        <div class="entry-image">
-            <?php if(is_single()){ ?>
-                <?php $thumb_url = wp_get_attachment_image_src(get_post_thumbnail_id(),'full', true); ?>
-                <a href="<?php echo esc_url($thumb_url[0]); ?>" title="<?php the_title(); ?>" class="prettyPhoto" rel="bookmark">
-                    <?php the_post_thumbnail('blog'); ?>
-                </a>
-            <?php } else { ?>
-                <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__('Permalink to %s', 'minti'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
-                    <?php the_post_thumbnail('blog'); ?>
-                </a>
-            <?php } ?>
-        </div>
-        <?php } ?>
-    <?php } ?>
+
         <?php if(is_single()){ ?>
         <div class="entry-meta">
             <?php get_template_part( 'framework/inc/meta' ); ?>
@@ -53,5 +38,20 @@
         <?php } ?>
 
     </div>
-
+  <?php if (!is_single() || (is_single() && get_post_meta( get_the_ID(), 'minti_hideimage', true ) == false)) { ?>
+        <?php if ( has_post_thumbnail() ) { ?>
+        <div class="entry-image">
+            <?php if(is_single()){ ?>
+                <?php $thumb_url = wp_get_attachment_image_src(get_post_thumbnail_id(),'full', true); ?>
+                <a href="<?php echo esc_url($thumb_url[0]); ?>" title="<?php the_title(); ?>" class="prettyPhoto" rel="bookmark">
+                    <?php the_post_thumbnail('blog'); ?>
+                </a>
+            <?php } else { ?>
+                <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__('Permalink to %s', 'minti'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
+                    <?php the_post_thumbnail('blog'); ?>
+                </a>
+            <?php } ?>
+        </div>
+        <?php } ?>
+    <?php } ?>
 </article><!-- #post -->
